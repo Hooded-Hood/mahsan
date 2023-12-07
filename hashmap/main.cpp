b53a5df96c478d9094496c2a9fd81cd9b8e11ac5
hadi <alikarami.hadi@gmail.com>
2023-12-07 23:48:37 +0330

    gitignore

--- hashmap/main.cpp
+++ hashmap/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
-#include "HashMap_lib/Bucket.h"
 #include "HashMap_lib/HashMap.h"
 
 using namespace std;
 
-
 int main() {
-    cout << "Hello World!";
+    HashMap<int, int> mp;
+    mp.set(2, 2);
+    cout << mp[2];
     return 0;
 }
